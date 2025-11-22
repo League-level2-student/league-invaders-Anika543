1891c664df3ac7551b79ef0e2fae801e3ed81ea9
Aarit Jayaswal <aaritrj@gmail.com>
2025-11-22 11:30:15 -0800

    leagueInvaders runner

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame; 
+	public static final int WIDTH = 500; 
+	public static final int HEIGHT = 800; 
+	
+	
+	public static void main (String[] args) {
+		
+	}
+	public LeagueInvaders(JFrame frame) {
+		this.frame = frame; 
+	}
+	
+	void setup() {
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
